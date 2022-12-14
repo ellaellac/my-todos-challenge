479b00383e3b5c235775772054f646d4c77661eb
ellaellac <ellachoywy@gmail.com>
2022-12-14 12:49:15 +0000

    del btn - not finish

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import Button from "../Button/Button";
 import "./List.scss";
 
 const List = ({ userInput }) => {
+  //cross-box
   let inputClassName = "input-text";
   const [cross, setCross] = useState(false);
 
@@ -12,11 +14,33 @@ const List = ({ userInput }) => {
   if (cross) {
     inputClassName += "-cross";
   }
+  //Button- delete List
+  const [delList, setdelList] = useState(false);
+
+  const handleDel = () => {
+    setdelList(!delList);
+  };
+
+  //   const emptyListJSX = (
+  //     <>
+  //     </>
+  //   );
+
+  //   const showList = (
+  //     <div className="list">
+  //       <p onClick={handleCross}> ▢ </p>
+  //       <p className={inputClassName}> {userInput}</p>
+  //       <Button title="Delete" btnFunction={handleDel} />
+  //       <>{/* {delList ?emptyListJSX :} */}</>
+  //     </div>
+  //   );
 
   return (
     <div className="list">
       <p onClick={handleCross}> ▢ </p>
       <p className={inputClassName}> {userInput}</p>
+      <Button title="Delete" btnFunction={handleDel} />
+      {/* {delList ? emptyListJSX : showList} */}
     </div>
   );
 };
